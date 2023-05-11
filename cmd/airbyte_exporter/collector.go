e60ec554b0d01721e097a9875c07ce431323e351
VirtualTam <virtualtam@flibidi.net>
2023-05-11 23:45:14 +0200

    exporter/collector: expose pending/running job metrics
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- cmd/airbyte_exporter/collector.go
+++ cmd/airbyte_exporter/collector.go
@@ -10,16 +10,45 @@ const (
 	namespace = "airbyte"
 )
 
-type collector struct{}
+// collector collects and exposes Airbyte metrics.
+type collector struct {
+	// Airbyte jobs
+	jobPending        *prometheus.Desc
+	jobRunning        *prometheus.Desc
+	jobRunningOrphans *prometheus.Desc
+}
 
 // NewCollector initializes and returns a Prometheus collector for Airbyte metrics.
 func NewCollector() *collector {
-	return &collector{}
+	return &collector{
+		jobPending: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "job_pending"),
+			"Pending jobs",
+			nil,
+			nil,
+		),
+		jobRunning: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "job_running"),
+			"Running jobs",
+			nil,
+			nil,
+		),
+		jobRunningOrphans: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "job_running_orphan"),
+			"Running jobs associated with an inactive or deprecated connection",
+			nil,
+			nil,
+		),
+	}
 }
 
 // Describe publishes the description of each Airbyte metric to a metrics
 // channel.
-func (c *collector) Describe(ch chan<- *prometheus.Desc) {}
+func (c *collector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.jobPending
+	ch <- c.jobRunning
+	ch <- c.jobRunningOrphans
+}
 
 // Collect gathers metrics from Airbyte.
 func (c *collector) Collect(ch chan<- prometheus.Metric) {}
